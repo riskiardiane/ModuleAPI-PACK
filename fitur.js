5143da80f0d1b94d166089d4748575087783fa8c
InDsToReE <indstoredevelop@gmail.com>
2026-04-17 11:40:01 +0700

    refactor: move lyrics scraper to music directory

--- fitur.js
+++ fitur.js
@@ -2,8 +2,8 @@ const { tiktokdl2 } = require("./data/downloader/ssstiktok");
 const brat = require("./data/fitur/brat");
 const animku = require("./data/watch/animku");
 const lk21 = require("./data/watch/lk21");
-const lyrics = require("./data/watch/lyrics");
-const quran = require("./data/watch/quran");
+const lyrics = require("./data/music/lyrics");
+const quran = require("./data/islami/quran");
 
 module.exports = {
   tiktok: tiktokdl2,
